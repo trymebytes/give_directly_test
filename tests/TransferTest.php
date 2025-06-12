9ba7abdfae16d402faff73e776fca7d01ce09803
Tosin Oguntuyi <tcubed03@gmail.com>
2025-06-12 15:10:00 +0100

    Rename test methods and run phpcs

--- tests/TransferTest.php
+++ tests/TransferTest.php
@@ -1,24 +1,29 @@
 <?php
+
 use PHPUnit\Framework\TestCase;
 use GiveDirectly\TransferGenerator;
 
-class TransferTest extends TestCase {
-    private function transferGenerator(): TransferGenerator {
+class TransferTest extends TestCase
+{
+    private function transferGenerator(): TransferGenerator
+    {
         return new TransferGenerator();
     }
 
-    public function test_weekly_transfer(): void {
-        $actual_result = $this->transferGenerator()->generateTransfer("weekly", "2024-04-20", 100.00, 3);
+    public function test_weekly_transfer(): void
+    {
+        $actual_result   = $this->transferGenerator()->generateTransfer("weekly", "2024-04-20", 100.00, 3);
         $expected_result = [
-                ['date' => '2024-04-20', 'amount' => 100.00],
-                ['date' => '2024-04-27', 'amount' => 100.00],
-                ['date' => '2024-05-04', 'amount' => 100.00],
+            ['date' => '2024-04-20', 'amount' => 100.00],
+            ['date' => '2024-04-27', 'amount' => 100.00],
+            ['date' => '2024-05-04', 'amount' => 100.00],
         ];
         $this->assertEquals($expected_result, $actual_result);
     }
 
-    public function test_monthly_transfer(): void {
-        $actual_result = $this->transferGenerator()->generateTransfer("monthly", "2024-03-20", 100.00, 3);
+    public function test_monthly_transfer(): void
+    {
+        $actual_result   = $this->transferGenerator()->generateTransfer("monthly", "2024-03-20", 100.00, 3);
         $expected_result = [
             ['date' => '2024-03-20', 'amount' => 100.00],
             ['date' => '2024-04-20', 'amount' => 100.00],
@@ -27,8 +32,9 @@ class TransferTest extends TestCase {
         $this->assertEquals($expected_result, $actual_result);
     }
 
-    public function test_transfer_for_one_irregular_months(): void {
-        $actual_result = $this->transferGenerator()->generateTransfer("monthly", "2024-01-30", 100.00, 3);
+    public function test_monthly_transfer_for_single_irregular_months(): void
+    {
+        $actual_result   = $this->transferGenerator()->generateTransfer("monthly", "2024-01-30", 100.00, 3);
         $expected_result = [
             ['date' => '2024-01-30', 'amount' => 100.00],
             ['date' => '2024-02-29', 'amount' => 100.00],
@@ -38,8 +44,9 @@ class TransferTest extends TestCase {
     }
 
 
-    public function test_transfer_for__multiple_irregular_months(): void {
-        $actual_result = $this->transferGenerator()->generateTransfer("monthly", "2024-01-31", 100.00, 7);
+    public function test_monthly_transfer_for_multiple_irregular_months(): void
+    {
+        $actual_result   = $this->transferGenerator()->generateTransfer("monthly", "2024-01-31", 100.00, 7);
         $expected_result = [
             ['date' => '2024-01-31', 'amount' => 100.00],
             ['date' => '2024-02-29', 'amount' => 100.00],
@@ -52,8 +59,9 @@ class TransferTest extends TestCase {
         $this->assertEquals($expected_result, $actual_result);
     }
 
-    public function test_quarterly_transfer(): void {
-        $actual_result = $this->transferGenerator()->generateTransfer("quarterly", "2024-12-31", 100.00, 6);
+    public function test_quarterly_transfer(): void
+    {
+        $actual_result   = $this->transferGenerator()->generateTransfer("quarterly", "2024-12-31", 100.00, 6);
         $expected_result = [
             ['date' => '2024-12-31', 'amount' => 100.00],
             ['date' => '2025-03-31', 'amount' => 100.00],
@@ -65,8 +73,9 @@ class TransferTest extends TestCase {
         $this->assertEquals($expected_result, $actual_result);
     }
 
-    public function test_biannual_transfer(): void {
-        $actual_result = $this->transferGenerator()->generateTransfer("biannual", "2024-01-01", 100.00, 2);
+    public function test_biannual_transfer(): void
+    {
+        $actual_result   = $this->transferGenerator()->generateTransfer("biannual", "2024-01-01", 100.00, 2);
         $expected_result = [
             ['date' => '2024-01-01', 'amount' => 100.00],
             ['date' => '2024-07-01', 'amount' => 100.00],
@@ -74,13 +83,13 @@ class TransferTest extends TestCase {
         $this->assertEquals($expected_result, $actual_result);
     }
 
-    public function test_annual_transfer(): void {
-        $actual_result = $this->transferGenerator()->generateTransfer("annual", "2024-01-01", 100.00, 2);
+    public function test_annual_transfer(): void
+    {
+        $actual_result   = $this->transferGenerator()->generateTransfer("annual", "2024-01-01", 100.00, 2);
         $expected_result = [
             ['date' => '2024-01-01', 'amount' => 100.00],
             ['date' => '2025-01-01', 'amount' => 100.00],
         ];
         $this->assertEquals($expected_result, $actual_result);
     }
-    
 }
