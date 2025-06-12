8a6ff7a0891d8c02aa2504bc8a0c19476d76683e
Tosin Oguntuyi <tcubed03@gmail.com>
2025-06-12 07:34:55 +0100

    Add test for months with irregular number of days

--- tests/TransferTest.php
+++ tests/TransferTest.php
@@ -25,7 +25,31 @@ class TransferTest extends TestCase {
             ['date' => '2024-05-20', 'amount' => 100.00],
         ];
         $this->assertEquals($expected_result, $actual_result);
+    }
 
+    public function test_transfer_for_one_irregular_months(): void {
+        $actual_result = $this->transferGenerator()->generateTransfer("monthly", "2024-01-30", 100.00, 3);
+        $expected_result = [
+            ['date' => '2024-01-30', 'amount' => 100.00],
+            ['date' => '2024-02-29', 'amount' => 100.00],
+            ['date' => '2024-03-30', 'amount' => 100.00],
+        ];
+        $this->assertEquals($expected_result, $actual_result);
+    }
+
+
+    public function test_transfer_for__multiple_irregular_months(): void {
+        $actual_result = $this->transferGenerator()->generateTransfer("monthly", "2024-01-31", 100.00, 7);
+        $expected_result = [
+            ['date' => '2024-01-31', 'amount' => 100.00],
+            ['date' => '2024-02-29', 'amount' => 100.00],
+            ['date' => '2024-03-31', 'amount' => 100.00],
+            ['date' => '2024-04-30', 'amount' => 100.00],
+            ['date' => '2024-05-31', 'amount' => 100.00],
+            ['date' => '2024-06-30', 'amount' => 100.00],
+            ['date' => '2024-07-31', 'amount' => 100.00],
+        ];
+        $this->assertEquals($expected_result, $actual_result);
     }
     
 }
